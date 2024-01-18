950565731e10ca0e911593de6a1de453fbdeb17c
zhuotuo <zhuotuo@leagsoft.com>
2024-01-18 19:53:02 +0800

    commander基本使用

--- shuibuzhuo-test/lib/index.js
+++ shuibuzhuo-test/lib/index.js
@@ -1,72 +1,10 @@
 #!/usr/bin/env node
 
-const yargs = require("yargs/yargs");
-const dedent = require("dedent");
-const pkg = require("../package.json");
+const commander = require('commander')
+const pkg = require('../package.json')
 
-const argv = process.argv.slice(2);
-const cli = yargs();
+// const { program } = commander
 
-const context = {
-  shuibuzhuoVersion: pkg.version,
-};
+const program = new commander.Command()
 
-cli
-  .usage("Usage: shuibuzhuo-test [command] <options>")
-  .demandCommand(
-    1,
-    "A command is required. Pass --help to see all available commands and options."
-  )
-  .strict()
-  .recommendCommands()
-  .fail((err, msg) => {
-    console.log(err);
-  })
-  .alias("h", "help")
-  .alias("v", "version")
-  .wrap(cli.terminalWidth())
-  .epilogue(
-    dedent`
-  When a command fails, all logs are written to lerna-debug.log in the current working directory.
-
-  For more information, check out the docs at https://lerna.js.org/docs/introduction
-`
-  )
-  .options({
-    debug: {
-      type: "boolean",
-      describe: "Bootstrap debug mode",
-      alias: "d",
-    },
-  })
-  .option("registry", {
-    type: "string",
-    describe: "Define global registry",
-    alias: "r",
-  })
-  .group(["debug"], "Dev Options:")
-  .group(["registry"], "Extra Options:")
-  .command(
-    "init [name]",
-    "Do init a project",
-    (yargs) => {
-      yargs.option("name", {
-        type: "string",
-        describe: "Name of a project",
-        alias: "n",
-      });
-    },
-    (argv) => {
-      console.log("argv", argv);
-    }
-  )
-  .command({
-    command: "list",
-    alias: ["ls", "la", "ll"],
-    describe: "List local packages",
-    builder: (yargs) => {},
-    handler: (argv) => {
-      console.log("argv", argv);
-    },
-  })
-  .parse(argv, context);
+program.version(pkg.version).parse(process.argv)
