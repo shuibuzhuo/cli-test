6a7edbfa9928de559ffb9b2396427cbdaeca3c91
zhuotuo <zhuotuo@leagsoft.com>
2024-01-19 16:31:27 +0800

    feat: 全局配置

--- shuibuzhuo-test/lib/index.js
+++ shuibuzhuo-test/lib/index.js
@@ -7,4 +7,12 @@ const pkg = require('../package.json')
 
 const program = new commander.Command()
 
-program.version(pkg.version).parse(process.argv)
+program
+  .name(Object.keys(pkg.bin)[0])
+  .usage('<command> [options]')
+  .version(pkg.version)
+  .option('-d, --debug', '是否开启调试模式', false)
+  .option('-e, --envName <envName>', '获取环境变量名称')
+  .parse(process.argv);
+
+console.log(program.opts());
