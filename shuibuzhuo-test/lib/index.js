c3f96ec0ad43c582936f67576167a1c3fbc3eca7
zhuotuo <zhuotuo@leagsoft.com>
2024-01-19 16:49:59 +0800

    feat: 命令注册的2种方法

--- shuibuzhuo-test/lib/index.js
+++ shuibuzhuo-test/lib/index.js
@@ -12,7 +12,33 @@ program
   .usage('<command> [options]')
   .version(pkg.version)
   .option('-d, --debug', '是否开启调试模式', false)
-  .option('-e, --envName <envName>', '获取环境变量名称')
-  .parse(process.argv);
+  .option('-e, --envName <envName>', '获取环境变量名称');
+  
 
-console.log(program.opts());
+// command 注册命令
+const clone = program.command('clone <source> [destination]')
+clone
+  .description('clone a repo')
+  .option('-f, --force', '是否强制克隆')
+  .action((source, destination, cmdObj) => {
+    console.log('do clone', source, destination, cmdObj.force);
+  })
+
+// addCommand 注册子命令
+const service = new commander.Command('service')
+service
+  .command('start [port]')
+  .description('start service at some port')
+  .action(port => {
+    console.log('start service at port', port);
+  })
+service
+  .command('stop')
+  .description('stop service')
+  .action(() => {
+    console.log('stop service');
+  })
+  
+program.addCommand(service)
+
+program.parse(process.argv);
