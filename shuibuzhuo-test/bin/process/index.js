080b38bd6a58b6e478703cd166bf5b7ede6377a9
zhuotuo <zhuotuo@leagsoft.com>
2024-01-25 16:56:39 +0800

    spawn

--- shuibuzhuo-test/bin/process/index.js
+++ shuibuzhuo-test/bin/process/index.js
@@ -1,13 +1,33 @@
 const cp = require('child_process')
 const path = require('path')
 
-cp.exec('ls -al|grep node_modules', function(err, stdout, stderr) {
-  console.log(err);
-  console.log(stdout);
-  console.log(stderr);
-})
+// cp.exec('ls -al|grep node_modules', function(err, stdout, stderr) {
+//   console.log(err);
+//   console.log(stdout);
+//   console.log(stderr);
+// })
 
 // cp.execFile(path.resolve(__dirname, 'test.shell'), function(err, stdout, stderr) {
+//   console.log(err);
+//   // console.log(stdout);
+//   // console.log(stderr);
+// })
+
+// const child = cp.spawn('npm.cmd', ['install'], {
+//   cwd: path.resolve('E:/vue/cli-test/shuibuzhuo-test-lib')
+// })
+
+// child.stdout.on('data', function(chunk) {
+//   console.log('stdout', chunk.toString());
+// })
+
+// child.stderr.on('data', function(chunk) {
+//   console.log('stderr', chunk.toString());
+// })
+
+// cp.exec('npm install', {
+//   cwd: path.resolve('E:/vue/cli-test/shuibuzhuo-test-lib')
+// }, function(err, stdout, stderr) {
 //   console.log(err);
 //   console.log(stdout);
 //   console.log(stderr);
