bfa1c7a368d277eec1164385eabd1eddb12b4901
shuibuzhuo <2301140231@qq.com>
2024-01-28 10:39:23 +0800

    stdin: inherit

--- shuibuzhuo-test/bin/process/index.js
+++ shuibuzhuo-test/bin/process/index.js
@@ -1,5 +1,5 @@
-const cp = require('child_process')
-const path = require('path')
+const cp = require("child_process");
+const path = require("path");
 
 // cp.exec('ls -al|grep node_modules', function(err, stdout, stderr) {
 //   console.log(err);
@@ -13,17 +13,20 @@ const path = require('path')
 //   // console.log(stderr);
 // })
 
-// const child = cp.spawn('npm.cmd', ['install'], {
-//   cwd: path.resolve('E:/vue/cli-test/shuibuzhuo-test-lib')
-// })
+const child = cp.spawn("npm", ["install"], {
+  cwd: path.resolve(
+    "/Users/tuomatuo/Documents/code/cli-test/shuibuzhuo-test-lib"
+  ),
+  stdio: "inherit",
+});
 
-// child.stdout.on('data', function(chunk) {
-//   console.log('stdout', chunk.toString());
-// })
+// child.stdout.on("data", function (chunk) {
+//   console.log("stdout", chunk.toString());
+// });
 
-// child.stderr.on('data', function(chunk) {
-//   console.log('stderr', chunk.toString());
-// })
+// child.stderr.on("data", function (chunk) {
+//   console.log("stderr", chunk.toString());
+// });
 
 // cp.exec('npm install', {
 //   cwd: path.resolve('E:/vue/cli-test/shuibuzhuo-test-lib')
@@ -33,9 +36,9 @@ const path = require('path')
 //   console.log(stderr);
 // })
 
-const child = cp.fork(path.resolve(__dirname, 'child.js'))
-child.send('hello from main')
-child.on('message', (msg) => {
-  console.log('main receive message', msg);
-})
-console.log('main pid: ', process.pid);
+// const child = cp.fork(path.resolve(__dirname, 'child.js'))
+// child.send('hello from main')
+// child.on('message', (msg) => {
+//   console.log('main receive message', msg);
+// })
+// console.log('main pid: ', process.pid);
