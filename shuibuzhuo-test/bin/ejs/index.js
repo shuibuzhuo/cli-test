be3f1a3bae5e6692cf766695bb87f0424feb06b7
shuibuzhuo <2301140231@qq.com>
2024-02-03 15:04:49 +0800

    ejs 不同标签用法

--- shuibuzhuo-test/bin/ejs/index.js
+++ shuibuzhuo-test/bin/ejs/index.js
@@ -3,41 +3,57 @@ const ejs = require("ejs");
 
 const html = "<div><%= user.name %></div>";
 const options = {};
+// const data = {
+//   user: {
+//     name: "hamo123",
+//   },
+// };
+// const data2 = {
+//   user: {
+//     name: "zhangsan123",
+//   },
+// };
+
+// // 返回 compiled function，用于解析 html 中的 ejs 模板
+// const template = ejs.compile(html, options);
+// const compiledTemplate = template(data);
+// const compiledTemplate2 = template(data2);
+// console.log(compiledTemplate);
+// console.log(compiledTemplate2);
+
+// // 第二种用法
+// const renderedTemplate = ejs.render(html, data, options);
+// console.log(renderedTemplate);
+
+// // 第三种用法
+// // 3.1 Promise
+// const renderedFile = ejs.renderFile(
+//   path.resolve(__dirname, "template.html"),
+//   data,
+//   options
+// );
+// renderedFile.then((file) => console.log(file));
+// renderedFile.then(console.log);
+
+// // 3.2 callback
+// ejs.renderFile(
+//   path.resolve(__dirname, "template.html"),
+//   data,
+//   options,
+//   (err, file) => {
+//     console.log(file);
+//   }
+// );
+
 const data = {
   user: {
-    name: "hamo123",
-  },
-};
-const data2 = {
-  user: {
-    name: "zhangsan123",
+    name: "hamo",
+    nickname: '<div>哈默</div>'
   },
 };
 
-// 返回 compiled function，用于解析 html 中的 ejs 模板
-const template = ejs.compile(html, options);
-const compiledTemplate = template(data);
-const compiledTemplate2 = template(data2);
-console.log(compiledTemplate);
-console.log(compiledTemplate2);
-
-// 第二种用法
-const renderedTemplate = ejs.render(html, data, options);
-console.log(renderedTemplate);
-
-// 第三种用法
-// 3.1 Promise
-const renderedFile = ejs.renderFile(
-  path.resolve(__dirname, "template.html"),
-  data,
-  options
-);
-renderedFile.then((file) => console.log(file));
-renderedFile.then(console.log);
-
-// 3.2 callback
 ejs.renderFile(
-  path.resolve(__dirname, "template.html"),
+  path.resolve(__dirname, "template2.html"),
   data,
   options,
   (err, file) => {
